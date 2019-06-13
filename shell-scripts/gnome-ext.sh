43fd09fdeb9c414f5e7f850cb644c8a81c9c67b0
Nikhil Thomas <nikhilthomas1@gmail.com>
2019-06-13 09:36:51 +0530

    Add a script to unzip and enable gnome extensions

---
+++ shell-scripts/gnome-ext.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Download Gnome extensions
+# https://extensions.gnome.org/
+
+# usage
+# gnome-ext.sh <path-to-downloaded-extension>
+
+EXT_PATH=$1
+echo $EXT_PATH
+
+EXT_UUID=$(unzip -c ${EXT_PATH} metadata.json | grep uuid | cut -d \" -f 4)
+echo $EXT_UUID
+
+GNOME_EXT_PATH=~/.local/share/gnome-shell/extensions/${EXT_UUID}
+
+echo $GNOME_EXT_PATH
+mkdir -p ${GNOME_EXT_PATH}
+
+unzip -q ${EXT_PATH} -d $GNOME_EXT_PATH
+
+gnome-shell-extension-tool -e ${EXT_UUID}
+
