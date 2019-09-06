56e68cdbb3e833fe51dee899f7ef8b39a374f66d
Nikhil Thomas <nikhilthomas1@gmail.com>
2019-09-06 17:17:28 +0530

    Update pmdr script

--- cybersecurity-ops-with-bash/02_bash-primer/04_for-loop-2.sh
+++ cybersecurity-ops-with-bash/02_bash-primer/04_for-loop-2.sh
@@ -9,3 +9,14 @@ for item in $(ls | grep sh) {1..5}; do
   echo $item
 done
 
+echo "#########"
+
+for i in {99..106..2}; do
+  echo $i
+done
+
+echo "#########"
+
+for i in {099..106..2}; do
+  echo $i
+done
